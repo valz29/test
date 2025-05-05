c62cdb9212a6cc64e13c4a36fcfdf0a5b54049d3
valentino <avrg2005@hotmail.com>
2025-05-05 02:28:40 -0500

    Primer commit

---
+++ Clasificacion.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:54df719873133a4aed20655a3f43701567344de78fd26418dcf9b9465c76c1c1
+size 5451
---
+++ prueba.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f39a3cd3b726ae253334e71f226dfd50a7482b05060221641da40dc51dd1fad5
+size 2206
